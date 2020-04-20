5295789c6eec5cd2787d33465ccd2ce0110a65a5
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2020-04-20 12:06:20 +0200

    initial setup

---
+++ router/contentRouter.js
@@ -0,0 +1,18 @@
+const express = require("express")
+
+const contentController = require("../controller/contentController")
+
+const router = express.Router()
+
+router
+  .route("/")
+  .post(contentController.createContentSet)
+  .get(contentController.getAllContentSets)
+
+router
+  .route("/:dataset")
+  .get(contentController.getContentSet)
+  .put(contentController.replaceContentSet)
+  .patch(contentController.updateContentSet)
+
+module.exports = router
