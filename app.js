40316c3435e0915b3d773065d9e9095b2c66ba05
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2020-05-15 23:20:35 +0200

    error handling middleware applied

--- app.js
+++ app.js
@@ -20,7 +20,22 @@ app.use(express.json()) // <-- body parser
 // -- routing
 app.use("/api/v1", contentRouter)
 app.use("/api/v1/users", userRouter)
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    messsage: `Couldn't find ${req.originalUrl}`,
+  })
+  next()
+})
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500
+  err.status = err.status || "error"
 
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  })
+})
 const DB = process.env.DB_CONNECTION_STRING.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
--- controller/authController.js
+++ controller/authController.js
@@ -1,12 +1,28 @@
 const User = require("../model/userModel")
+const jwt = require("jsonwebtoken")
 
 exports.signup = async (req, res, next) => {
-  console.log("signup controller called")
   try {
-    const newUser = await User.create(req.body)
+    // const newUser = await User.create(req.body)
+    // above line deprecieted for security reasons
+    // (user permission tier can be manually inserted e.g.)
+    // use object literal instead to pass only fields we want:
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      login: req.body.login,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm,
+    })
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    })
+
     res.status(201).json({
       status: "success",
-      data: newUser,
+      token,
+      data: { user: newUser },
     })
   } catch (err) {
     res.status(500).json({
@@ -15,3 +31,5 @@ exports.signup = async (req, res, next) => {
     })
   }
 }
+
+// exports.login = (req, res, next)
