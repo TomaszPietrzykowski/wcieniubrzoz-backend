637917785cd81e451ee921ba592205421f4a95ab
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2020-05-24 16:18:46 +0200

    email form authentication added

--- controller/emailController.js
+++ controller/emailController.js
@@ -10,41 +10,45 @@ exports.pingEmail = catchAsync(async (req, res, next) => {
 })
 
 exports.sendEmail = catchAsync(async (req, res, next) => {
-  const output = `
+  if (req.headers.auth === process.env.EMAIL_FORM_AUTH) {
+    const output = `
 <h4>Wiadomość z formularza W cieniu brzóz:</4>
 <p>Od: ${req.body.name}</p>
 <p>email: ${req.body.email}</p>
 <h3>${req.body.title}</h3>
 <p>${req.body.message}</p>
 `
-  const txt = `Wiadomość ze strony W cieniu brzóz:\n\n Od: ${req.body.name}\n email: ${req.body.email} \n\n${req.body.message}`
+    const txt = `Wiadomość ze strony W cieniu brzóz:\n\n Od: ${req.body.name}\n email: ${req.body.email} \n\n${req.body.message}`
 
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "mail.wcieniubrzoz.pl",
-    port: 465,
-    secure: true,
-    auth: {
-      user: "kontakt@wcieniubrzoz.pl",
-      pass: "saranda123",
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
-  })
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "mail.wcieniubrzoz.pl",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "kontakt@wcieniubrzoz.pl",
+        pass: "saranda123",
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    })
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: "W cieniu brzóz...<kontakt@wcieniubrzoz.pl>", // sender address <-- has to be valid for google!!!
-    to: "pietrzykowski77@gmail.com, danuta.pietrzykowska@gmail.com", // list of receivers
-    subject: `Ktoś wysłał do Ciebie wiadomość :)`, // Subject line
-    text: txt, // plain text body
-    html: output, // html body
-  })
-  res.status(200).json({
-    status: "success",
-    message: "email sent successfuly",
-    envelope: info.envelope,
-    accepted: info.accepted,
-  })
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: "W cieniu brzóz...<kontakt@wcieniubrzoz.pl>", // sender address <-- has to be valid for google!!!
+      to: "pietrzykowski77@gmail.com", // list of receivers
+      subject: `Ktoś wysłał do Ciebie wiadomość :)`, // Subject line
+      text: txt, // plain text body
+      html: output, // html body
+    })
+    res.status(200).json({
+      status: "success",
+      message: "Email sent successfuly",
+      envelope: info.envelope,
+      accepted: info.accepted,
+    })
+  } else {
+    return next(new AppError("Email form authorization string rejected", 401))
+  }
 })
